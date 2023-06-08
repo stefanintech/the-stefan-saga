9e6cb18ea72bfc26fdfbdbc6a11352923a42b0c4
Stefan <stefantaitano@gmail.com>
2023-06-07 23:39:41 -0600

    connect database to mongo

--- app.js
+++ app.js
@@ -3,8 +3,13 @@ require('dotenv').config()
 const express = require('express')
 const expressLayout = require('express-ejs-layouts')
 
+const connectDB = require('./server/config/db')
+
 const app = express();
 const PORT = process.env.PORT || 5000
+
+// Connect DB
+connectDB()
 
 app.use(express.static('public'))
 
