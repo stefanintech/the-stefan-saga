8a79d83fe2c564630050265ed3c40695a8e2bcc3
Stefan <stefantaitano@gmail.com>
2023-06-09 21:19:07 -0600

    add pagination

--- server/routes/main.js
+++ server/routes/main.js
@@ -8,22 +8,37 @@ const Post = require('../models/Post')
  */
 
 router.get('', async (req,res) => {
-
-    const locals = {
-        title: 'The Stefan Saga',
-        description: 'Blog created with love.'
-    }
-
     try {
-        const data = await Post.find();
-        res.render('index', {locals, data});
+        const locals = {
+            title: 'The Stefan Saga',
+            description: 'Blog created with love.'
+        }
+        
+        let perPage = 10;
+        let page =req.query.page || 1;
+        
+        const data = await Post.aggregate([{ $sort: {createdAt: -1} } ])
+        .skip(perPage * page - perPage)
+        .limit(perPage)
+        .exec();
+
+        const count = await Post.count();
+        const nextPage = parseInt(page) + 1;
+        const hasNextPage = nextPage <= Math.ceil(count / perPage);
+        
+        res.render('index', {
+            locals, 
+            data,
+            current: page,
+            nextPage: hasNextPage ? nextPage : null
+        });
+        
+        
+        
+        
     } catch (error) {
         console.log(error)
     }
-
-    
-
-    
 })
 
 
