8024e866318dcb8e084b13c6121b2a0f6aae1fe2
Stefan <stefantaitano@gmail.com>
2023-06-09 22:17:57 -0600

    add middleware for search

--- app.js
+++ app.js
@@ -11,6 +11,9 @@ const PORT = process.env.PORT || 5000
 // Connect DB
 connectDB()
 
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
 app.use(express.static('public'))
 
 // Templating Engine
--- server/routes/main.js
+++ server/routes/main.js
@@ -71,6 +71,40 @@ router.get('/post/:id', async (req, res) => {
   });
 
 
+/**
+ * POST /
+ * Post - searchTerm
+*/
+router.post('/search', async (req, res) => {
+  try {
+    const locals = {
+      title: "Search",
+      description: "Simple Blog created with NodeJs, Express & MongoDb."
+    }
+
+    let searchTerm = req.body.searchTerm;
+    const searchNoSpecialChar = searchTerm.replace(/[^a-zA-Z0-9 ]/g, "")
+
+    const data = await Post.find({
+      $or: [
+        { title: { $regex: new RegExp(searchNoSpecialChar, 'i') }},
+        { body: { $regex: new RegExp(searchNoSpecialChar, 'i') }}
+      ]
+    });
+
+    res.render("search", {
+      data,
+      locals,
+      currentRoute: '/'
+    });
+
+  } catch (error) {
+    console.log(error);
+  }
+
+});
+
+
 
 
 router.get('/about', (req,res) => {
